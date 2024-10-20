f87b2fd29ff8395ea87141550b5680b59caca77d
Jeverson Santos <jeverson.apsantos@gmail.com>
2024-10-20 17:47:09 -0300

    edtições 20/10

--- Program.cs
+++ Program.cs
@@ -1,9 +1,18 @@
 
-
-
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using minimal_api.Dominio.DTOS;
+using minimal_api.Infraestrutura.Db;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DbContexto>(options => {
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("mysql"),
+        ServerVersion.AutoDetect(connectionString: builder.Configuration.GetConnectionString("mysql"))
+    );
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
